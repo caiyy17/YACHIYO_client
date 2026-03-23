10a2a4bdf982e36c276bf6fb4ce437571ddd7353
cyy <caiyiyi1998@gmail.com>
2026-03-23 23:30:00 +0900

    server port

--- Assets/Custom/YACHIO/Setting/GameSettings.cs
+++ Assets/Custom/YACHIO/Setting/GameSettings.cs
@@ -4,8 +4,8 @@ using System.Collections.Generic;
 [CreateAssetMenu(fileName = "GameSettingsData", menuName = "YACHIO/GameSettingsData", order = 1)]
 public class GameSettingsData : ScriptableObject
 {
-    public string server_url = "localhost:5050";
-    public string webrtc_url = "localhost:5055";
+    public string server_url = "localhost:8910";
+    public string webrtc_url = "localhost:15168";
     public string user_id = "0";
     public string pipeline_config = "demo_config";
     public string char_id = "0";
